368e179cccec59638026b9dfdd77e801169b71ff
Hannah Lopez <hannahlopez@Hannahs-Mac-mini.local>
2024-04-16 15:03:34 -0400

    set up app.js

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const morgan = require("morgan");
+
+const path = require("node:path");
+
+const app = express();
+
+const PORT = 5001;
+
+const cors = require("cors");
+
+//MIDDLEWARE
+
+// const siteRoutes = require()
+// const teacherRoutes = require()
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// app.use(cors())
+
+
+//ROUTES
+
+app.get("/", (req, res, next) => {
+    res.status(200).json({success: {message: "Index Page"}});
+})
+
+// app.use("/", siteRoutes);
+// app.use("/teacher", teacherRoutes);
+
+app.listen(PORT, () => {
+    console.log(`The server is listening ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
+})
+
