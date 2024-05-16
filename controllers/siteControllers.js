3f9cdc9e51b0c967e09db5af5727c9e9928ae2b0
Hannah Lopez <hannahlopez@Hannahs-Mac-mini.local>
2024-05-16 14:38:07 -0400

    added loginLocalFailed, logoutRequest, signupRequest to siteControllers

--- controllers/siteControllers.js
+++ controllers/siteControllers.js
@@ -1,4 +1,49 @@
 //Signup, Login, Logout Controllers
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+
+const loginLocalFailed = (req, res, next) => {
+    res.status(401).json({error: {message: "Username or password is incorrect"}, statusCode:401});
+}
+
+const logoutRequest = (req, res, next) => {
+    req.logout((error) => {
+        if (error) {
+            res.status(400).json({error: {message: "Something went wrong!", statusCode: 400}});
+        };
+        res.status(200).json({success: {message: "User logged out!", statusCode: 200}});
+    })
+};
+
+const signupRequest = (req, res, next) => {
+    const {firstName, lastName, username, password} = req.body;
+    bcrypt.hash(password, 10, async (error, hashedPassword) => {
+        if (err) {
+            return next(error);
+        }
+        const newUser = new User({
+            firstName,
+            lastName,
+            username,
+            password: hashedPassword,
+        });
+        try {
+            await newUser.save()
+            req.login(newUser, (err) => {
+                if (err) {
+                res.status(400).json({error: {message: "Something went wrong while signing up!", statusCode: 400}});
+                }
+            })
+        } catch (error) {
+            if (error === 11000 && err.keyPattern.username){
+                res.status(400).json({error: {message: "Username already exists", statusCode: 400}})
+            } else {
+                res.status(500).json({error: {message: "Internal server error", statusCode: 500}});
+            }
+        }
+    });
+};
+
 
 
 const createTeacherAccount = async (req, res, next) => {
@@ -42,4 +87,4 @@ const deleteAccount = async (req, res, next) => {
     };
 
 
-module.exports = {createTeacherAccount, createStudentAccount, createMusicianAccount, deleteAccount}
+module.exports = {loginLocalFailed, logoutRequest, signupRequest, createTeacherAccount, createStudentAccount, createMusicianAccount, deleteAccount};
